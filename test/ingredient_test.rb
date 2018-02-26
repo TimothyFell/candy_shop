4a873a46773bfeec20cae564b154000c9420b1bc
Michael Dao <michael.dao@gmail.com>
2018-02-26 09:20:11 -0700

    Change test file to pass Strings not Integers.

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -5,13 +5,13 @@ require './lib/ingredient'
 class IngredientTest < Minitest::Test
 
   def test_it_exists
-    ingredient = Ingredient.new("Milk Chocolate", 10)
+    ingredient = Ingredient.new("Milk Chocolate", "10")
 
     assert_instance_of Ingredient, ingredient
   end
 
   def test_it_has_attributes
-    ingredient = Ingredient.new("Milk Chocolate", 10)
+    ingredient = Ingredient.new("Milk Chocolate", "10")
 
     assert_equal "Milk Chocolate", ingredient.name
     assert_equal 10, ingredient.cost
